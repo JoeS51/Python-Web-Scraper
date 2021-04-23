8e93eac60abab346d9f4af0ecc3838a7182b2367
JoeS51 <43276756+JoeS51@users.noreply.github.com>
2021-04-23 09:00:40 -0700

    Gets website data and prints

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Python Webscraper Test")
+import requests
+from bs4 import BeautifulSoup
+print("test")
+
+URL = "https://dojo.code.ninja/students/cn-wa-newcastle/#"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="status-bar-container")
+
+print(results.prettify())
